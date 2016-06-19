4116bebf51eee02d67c9b72672fdf4f7b902e9d4
Nitish Kulkarni <ni4lgi@gmail.com>
2016-06-19 18:37:00 +0530

    fix error with date formats

--- analytics.py
+++ analytics.py
@@ -3,7 +3,7 @@ import pandas as pd
 import constants
 
 def ts2dict( ts ):
-    return( { constants.DATES_KEY : [ d.isoformat() for d in ts.index ], constants.VALUES_KEY : list( ts.values ) });
+    return( { constants.DATES_KEY : list( ts.index ), constants.VALUES_KEY : list( ts.values ) });
 
 def dict2ts( ts ):
     return( pd.Series( ts[ constants.VALUES_KEY ], index=ts[ constants.DATES_KEY ] ) );
@@ -16,4 +16,3 @@ def tsreturn( ts, window ):
 def fundReturn( client, schemeCode, window ):
     nav = dict2ts( dbio.fundNav( client, schemeCode ) );
     return ts2dict( tsreturn( nav, window ) );
-    
