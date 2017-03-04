1dca805b581c6dfd20bd9087f8048f40e64529b8
Aditya Mehta <adityamehta1990@gmail.com>
2017-03-04 11:38:26 +0530

    super sped up transaction upload

--- entropy/datafeeds/txnUpload.py
+++ entropy/datafeeds/txnUpload.py
@@ -13,7 +13,7 @@ CAMS_TXN_COLS = ['MF_NAME', 'INVESTOR_NAME', 'PAN', 'FOLIO_NUMBER', 'PRODUCT_COD
         'TRADE_DATE', 'TRANSACTION_TYPE', 'DIVIDEND_RATE' 'AMOUNT', 'UNITS', 'PRICE', 'BROKER']
 
 FILTER_OUT_TXN_TYPES = [
-    'address updated from kra'
+    'address updated from kra',
     'registration of nominee',
     'registered'
 ]
@@ -30,28 +30,33 @@ def importFundTransactionsFromFile(client, portfolioId, fileName):
     for col in df.columns:
         if df[col].dtype == object:
             df[col] = df[col].str.strip()
-    txns = df.to_dict('records')
-    txns = [t for t in txns if not match.matchAnyIdentifier( \
-        t['TRANSACTION_TYPE'], FILTER_OUT_TXN_TYPES, processor=str.lower)]
+    # get valid txns
+    txns = df[df.apply(lambda x: not match.matchAnyIdentifier(x.TRANSACTION_TYPE, FILTER_OUT_TXN_TYPES), axis=1)]
     funds = fundData.fundList(client)
-    txnsToAdd = failedTxns = []
-    for txn in txns:
-        isCloseEnded = txn['TRANSACTION_TYPE'].lower().find('nfo') > -1
+    txnsToAdd = []
+    failedTxns = []
+    # grouping because there can be many transactions per fund
+    for (key, txnGrp) in txns.groupby(['MF_NAME', 'SCHEME_NAME']):
+        isCloseEnded = any([x.lower().find('nfo') > -1 for x in set(txns.TRANSACTION_TYPE)])
         txnInfo = {
-            fc.FUND_NAME_AMFI: fundData.fundNameProcessor(txn['SCHEME_NAME']),
-            fc.FUND_HOUSE: txn['MF_NAME'],
+            fc.FUND_NAME_AMFI: fundData.fundNameProcessor(key[1]),
+            fc.FUND_HOUSE: key[0],
             fc.FUND_TYPE: 'close ended schemes' if isCloseEnded else 'open ended schemes'
         }
         txnInfo = fundData.enrichFundInfo(txnInfo)
         try:
             fund = match.matchDictBest(txnInfo, funds, ac.ASSET_NAME, scorer=fuzz.token_sort_ratio)
+            # fall back to token set if token sort doesnt work
+            if not fund:
+                fund = match.matchDictBest(txnInfo, funds, ac.ASSET_NAME, scorer=fuzz.token_set_ratio)
             fundId = str(fund[dbclient.MONGO_ID])
             # the txn ID is dummy here, gets correctly calced in addManyTransactions
-            txnsToAdd.append(portfolioData.newTransaction(0, fundId, fund[ac.ASSET_NAME],\
-                txn['AMOUNT'], txn['UNITS'], dtu.parse(txn['TRADE_DATE']), txn['TRANSACTION_TYPE']))
+            for txn in txnGrp.itertuples():
+                txnsToAdd.append(portfolioData.newTransaction(0, fundId, fund[ac.ASSET_NAME],\
+                    txn.AMOUNT, txn.UNITS, dtu.parse(txn.TRADE_DATE), txn.TRANSACTION_TYPE))
             print('processed {}'.format(txnInfo[ac.ASSET_NAME]))
         except Exception:
-            failedTxns.append(txn)
+            failedTxns = failedTxns + txnGrp.to_dict('records')
     ack = portfolioData.addManyTransactions(client, portfolioId, txnsToAdd)
     if not ack:
         print('Failed to store transactions in db')
