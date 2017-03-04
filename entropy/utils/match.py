38eeee19ac2db8d0ba1c807fe701a29e71253c8c
Aditya Mehta <adityamehta1990@gmail.com>
2017-03-04 11:36:15 +0530

    default fuzz processor does everything we need

--- entropy/utils/match.py
+++ entropy/utils/match.py
@@ -4,16 +4,14 @@ from fuzzywuzzy import process
 
 FUZZ_SCORE_CUTOFF = 95
 
-def matchOneIdentifier(query, choices, scorer=fuzz.token_set_ratio, processor=str.lower):
+def matchOneIdentifier(query, choices, scorer=fuzz.token_set_ratio):
     '''returns fuzzy matched identifier if unique, else None'''
-    matches = process.extractBests(query, choices, scorer=scorer, \
-        score_cutoff=FUZZ_SCORE_CUTOFF, processor=processor)
+    matches = process.extractBests(query, choices, scorer=scorer, score_cutoff=FUZZ_SCORE_CUTOFF)
     return matches[0][0] if len(matches) == 1 else None
 
-def matchAnyIdentifier(query, choices, scorer=fuzz.token_set_ratio, processor=str.lower):
+def matchAnyIdentifier(query, choices, scorer=fuzz.token_set_ratio):
     '''returns True if any identifier from choices fuzzy matches with the query'''
-    matches = process.extractBests(query, choices, scorer=scorer, \
-            score_cutoff=FUZZ_SCORE_CUTOFF, processor=processor)
+    matches = process.extractBests(query, choices, scorer=scorer, score_cutoff=FUZZ_SCORE_CUTOFF)
     return len(matches) > 0
 
 # (?) key-value match can be done by using fuzz
@@ -22,7 +20,7 @@ def matchDictBest(query, choices, key=None, scorer=fuzz.token_set_ratio):
     Provided key is used to perform fuzzy match
     '''
     choices = [c for c in choices if len(set(query.items()) & set(c.items()))]
-    matches = [c for c in choices if scorer(query[key].lower(), c[key].lower()) > FUZZ_SCORE_CUTOFF]
+    matches = [c for c in choices if scorer(query[key], c[key]) > FUZZ_SCORE_CUTOFF]
     if len(matches) == 1:
         return matches[0]
     elif len(matches) > 1:
